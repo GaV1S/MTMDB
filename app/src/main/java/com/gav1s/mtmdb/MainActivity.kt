bf0d176f7917d7193ebaa7f5cd148a16dc1ec4a3
GaV1S <GaV1S@yandex.ru>
2022-01-15 14:12:53 +0300

    homework02 finished (add any files)

---
+++ app/src/main/java/com/gav1s/mtmdb/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gav1s.mtmdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gav1s.mtmdb.framework.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
