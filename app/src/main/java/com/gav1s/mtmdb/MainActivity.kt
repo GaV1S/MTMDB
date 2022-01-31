e4dd47f61fbc3fb57bb30a1ee6296b7784211d9b
GaV1S <GaV1S@yandex.ru>
2022-01-31 17:01:58 +0300

    homework06 finished

--- app/src/main/java/com/gav1s/mtmdb/MainActivity.kt
+++ app/src/main/java/com/gav1s/mtmdb/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.gav1s.mtmdb
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gav1s.mtmdb.framework.MainBroadcastReceiver
 import com.gav1s.mtmdb.framework.ui.main_fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,11 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
